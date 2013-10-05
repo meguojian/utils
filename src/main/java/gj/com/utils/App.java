1f277ec97efa2e5feff234603e0ec9f42b2375dc
guojian <meguojian@163.com>
2013-10-05 17:12:09 +0800

    init

---
+++ src/main/java/gj/com/utils/App.java
@@ -0,0 +1,13 @@
+package gj.com.utils;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
